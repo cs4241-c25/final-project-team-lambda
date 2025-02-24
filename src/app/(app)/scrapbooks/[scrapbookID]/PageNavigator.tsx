a9628099cc345b4db07fcf4c683cc1802afe7f5c
notsoli <samrandaofficial@gmail.com>
2025-02-23 21:16:09 -0500

    Added zoom

--- src/app/(app)/scrapbooks/[scrapbookID]/PageNavigator.tsx
+++ src/app/(app)/scrapbooks/[scrapbookID]/PageNavigator.tsx
@@ -7,6 +7,7 @@ export default function PageNavigator(
     { pages, appendPage }:
     { pages: Page[], appendPage: () => void } ) {
     const [ currentPage, setCurrentPage ] = useState(0);
+    const [ zoom, setZoom ] = useState(1);
 
     function addpage() {
         appendPage();
@@ -19,9 +20,16 @@ export default function PageNavigator(
                 <div className="w-6"></div> :
                 <button className="w-6 m-auto" onClick={() => setCurrentPage(currentPage - 1)}>{"<"}</button>
             }
-            <div className="flex flex-1 min-h-0 flex-col gap-2 overflow-auto">
-                <ScrapbookPage page={pages[currentPage]} />
-                <p className="text-center h-8">{pages[currentPage].number} / {pages.length}</p>
+            <div className="flex flex-1 min-h-0 flex-col gap-2">
+                <div className="flex gap-2 justify-center">
+                    <button onClick={() => setZoom(zoom / 1.1)}>-</button>
+                    <button onClick={() => setZoom(1)}>zoom</button>
+                    <button onClick={() => setZoom(zoom * 1.1)}>+</button>
+                </div>
+                <div className="min-h-0 w-full mb-auto overflow-auto">
+                    <ScrapbookPage page={pages[currentPage]} zoom={zoom} />
+                </div>
+                <p className="text-center h-8 shrink-0">{pages[currentPage].number} / {pages.length}</p>
             </div>
             { currentPage < pages.length - 1 ?
                 <button className="w-6 m-auto" onClick={() => setCurrentPage(currentPage + 1)}>{">"}</button> :
--- src/app/(app)/scrapbooks/[scrapbookID]/ScrapbookPage.tsx
+++ src/app/(app)/scrapbooks/[scrapbookID]/ScrapbookPage.tsx
@@ -1,6 +1,6 @@
 import { Page } from '@/lib/models';
 
-export default function ScrapbookPage({ page }: { page: Page }) {
+export default function ScrapbookPage({ page, zoom }: { page: Page, zoom: number }) {
     const size = {
         width: 17,
         height: 22
@@ -8,11 +8,12 @@ export default function ScrapbookPage({ page }: { page: Page }) {
     
     return (
         <div id="canvas" className={`
-            border-2 border-gray-300 rounded-sm
+            border-2 border-gray-300 rounded-sm mx-auto
             flex justify-center items-center text-gray-300
         `}
         style={{
-            aspectRatio: `${size.width} / ${size.height}`
+            aspectRatio: `${size.width} / ${size.height}`,
+            width: zoom * 100 + "%"
         }}>
             [canvas pg {page.number}]
         </div>
