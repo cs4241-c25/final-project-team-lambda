160738b4715ab106935616405967f247f1ec0bc3
ajaguiar <ajaguiar1007@gmail.com>
2025-02-24 12:45:31 -0500

    hello

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,7 +1,20 @@
-export default function Profile() {
+import { getSession } from "@/lib/auth";
+import {mockProviders, mockSession} from "next-auth/client/__tests__/helpers/mocks";
+//import user = mockSession.user;
+//import credentials = mockProviders.credentials;
+
+export default async function Profile() {
+    const session = await getSession();
+    const user = session
+    console.log(session.user.name);
+
     return (
         <main>
             <h1>Profile</h1>
+            <h2>Image: </h2>
+            <h2>Name: </h2>
+            <h2>Username: </h2>
+            <h2>Password: </h2>
         </main>
     );
 }
