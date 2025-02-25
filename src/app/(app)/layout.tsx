d89f26fe32f8b708b0c8acb2abac93b074e88e87
notsoli <samrandaofficial@gmail.com>
2025-02-25 11:56:24 -0500

    Modified navbars

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import Logout from "./Logout";
+import Link from "next/link";
 
 export default function AppLayout({
     children,
@@ -9,8 +10,9 @@ export default function AppLayout({
         <body className="flex flex-col h-screen">
             <nav>
                 <ul className="flex items-center gap-4 px-4 py-2">
-                    <li><a href="/scrapbooks" className="no-underline"><h2>LifeLog</h2></a></li>
-                    <li><a href="/profile" className="no-underline hover:underline">Profile</a></li>
+                    <li><Link href="/" className="no-underline"><h2>LifeLog</h2></Link></li>
+                    <li><Link href="/scrapbooks" className="no-underline hover:underline">Scrapbooks</Link></li>
+                    <li><Link href="/profile" className="no-underline hover:underline">Profile</Link></li>
                     <li className="ml-auto"><Logout /></li>
                 </ul>
             </nav>
