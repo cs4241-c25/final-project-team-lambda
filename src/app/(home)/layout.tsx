85e9a46347d29e3d492b118672b71ce909526626
notsoli <samrandaofficial@gmail.com>
2025-03-04 10:58:29 -0500

    Added homepage styling

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function HomeLayout({
     children,
@@ -6,12 +7,22 @@ export default function HomeLayout({
     children: React.ReactNode;
   }>) {
     return (
-        <body>
-            <nav>
-                <ul className="flex items-center gap-4 px-4 py-2 [&_a]no-underline">
-                    <li><Link href="/" className="no-underline"><h2>LifeLog</h2></Link></li>
-                    <li className="ml-auto"><Link href="/login" className="no-underline hover:underline">Login</Link></li>
-                    <li><Link href="/register" className="no-underline hover:underline">Register</Link></li>
+        <body className="flex flex-col h-screen">
+            <nav className="bg-[--mediumgreen]">
+                <ul className="flex items-center gap-4 [&_a]no-underline px-4 py-2">
+                    <li>
+                        <Link href="/" className="flex items-center gap-2 no-underline">
+                            <Image
+                                src="/images/logo-small.png"
+                                alt="The LifeLog logo depicting an outline of an open book."
+                                width="50"
+                                height="50"
+                            />
+                            <h2>LifeLog</h2>
+                        </Link>
+                    </li>
+                    <li className="ml-auto"><Link href="/login" className="bg-[--darkgreen] no-underline px-4 py-2 rounded-md hover:text-white">Log In</Link></li>
+                    <li><Link href="/register" className="bg-[--darkgreen] no-underline px-4 py-2 rounded-md hover:text-white">Register</Link></li>
                 </ul>
             </nav>
             {children}
